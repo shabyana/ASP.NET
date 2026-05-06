f2f29a74c70880c0bb0529ed4c87a19fb3761cb6
Яна <shabaya08@gmail.com>
2026-05-06 15:14:05 +0300

    razor L2 1-8

---
+++ RazorPagesMovie/RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>()
+                .Property(e => e.ReleaseDate)
+                .HasColumnType("timestamp without time zone"); 
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ RazorPagesMovie/RazorPagesMovie/Migrations/20260506114654_Rating.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "numeric(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
