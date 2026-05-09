9ad033c55c46ed6273b5047b9a2528dbb45baeb0
Яна <shabaya08@gmail.com>
2026-05-10 00:27:11 +0300

    MVC_4.1,2

---
+++ WebMVCR4.1,2/WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCR1.Models;
+
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static PersonRepository db = new PersonRepository();
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+
+
+        public ViewResult OutputData()
+        {
+            ViewBag.Pers = db.GetAllResponses;
+            ViewBag.Count = db.NumberOfPerson;
+            return View("ListPerson");
+        }
+
+
+        //public string Index(string hel)
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel;
+        //    return Greeting;
+
+        //}
+    }
+}
