ae0f87fca747a58d07f0aee568e9663afc30a568
Яна <shabaya08@gmail.com>
2026-05-10 00:28:45 +0300

    MVC_4.3

---
+++ MvcDataViews/MvcDataViews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcDataViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
